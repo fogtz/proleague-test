dcfb7365bbb1c2da12195b1d86448719a699da23
Leonardo Vicenzo <101267531+fogtz@users.noreply.github.com>
2026-01-16 18:47:01 -0300

    feat(Queue): implements queue module

--- server/main.lua
+++ server/main.lua
@@ -1,10 +1,34 @@
+local instances = {}
+
 --- Função para ativar/desativar a busca por uma partida.
 --- @param source number
 local function toggleSearch(source)
-    
+    local userPed = GetPlayerPed(source)
+    local userId = NetworkGetNetworkIdFromEntity(userPed)
+
+    if (not userId) then
+        return false, 'Jogador não encontrado.'
+    end
+
+    local queueState = instances.Queue:isInQueue(userId)
+    local success, message
+    if queueState then
+        success, message = instances.Queue:remove(userId)
+    else
+        success, message = instances.Queue:add(userId)
+    end
+    return success, message
 end
 
 --- Comando para ativar/desativar a busca por uma partida.
 RegisterCommand("x1", function(source, args, rawCommand)
-    toggleSearch(source)
+    local success, message = toggleSearch(source)
+    print(success, message)
 end, false)
+
+--- Thread inicial para definir instancias.
+Citizen.CreateThread(function()
+    if not instances.Queue then
+        instances.Queue = Queue.new()
+    end
+end)
---
+++ server/modules/Queue.lua
@@ -0,0 +1,63 @@
+Queue = {}
+Queue.__index = Queue
+
+--- Instancia a fila.
+--- @return table
+function Queue.new()
+    local self = setmetatable({}, Queue)
+    self.queuedPlayers = {}
+    return self
+end
+
+--- Adiciona um jogador à fila.
+--- @param userId number
+--- @return boolean, string 
+function Queue:add(userId)
+    if self.queuedPlayers[userId] then
+        return false, 'O jogador já está na fila.'
+    end
+
+    self.queuedPlayers[userId] = true
+    
+    local canMatch = self:checkMatch()
+    if canMatch then
+        
+    end
+    return true, 'Jogador adicionado à fila com sucesso.'
+end
+
+--- Remove um jogador da fila.
+--- @param userId number
+--- @return boolean, string
+function Queue:remove(userId)
+    if not self.queuedPlayers[userId] then
+        return false, 'O jogador não está na fila.'
+    end
+
+    self.queuedPlayers[userId] = nil
+    return true, 'Jogador removido da fila com sucesso.'
+end
+
+--- Função para verificar se há jogadores suficientes para iniciar uma partida.
+--- @return table | nil
+function Queue:checkMatch()
+    if (not self.queuedPlayers) then
+        return nil
+    end
+
+    local players = {}
+    for userId, _ in pairs(self.queuedPlayers) do
+        table.insert(players, userId)
+
+        if #players >= 2 then
+            return players
+        end
+    end
+    return nil
+end
+
+--- Verifica se um jogador está na fila.
+--- @param userId number
+function Queue:isInQueue(userId)
+    return self.queuedPlayers[userId] ~= nil
+end
